48b21f713ef7df0d73cbf650dd9f5966e5fe388a
lakatoshv <some.pochta1@gmail.com>
2018-08-18 16:30:07 +0300

    Додана модель Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use App\Posts;
 
 class PostsController extends Controller
 {
     public function index(){
-    	$posts = DB::table("posts")->get();
+    	$posts = Posts::all();
     	return view('index', compact('posts'));
     }
     public function show($id){
-    	$post = DB::table("posts")->find($id);
+    	$post = Posts::find($id);
 	    $comments = DB::table("comments")->where("post_id", $id)->get();
 	   	return view('posts.show', compact("post"), compact("comments"));
     }
