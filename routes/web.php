1042aec1346221820bd19c34f7dad3edb1aa5781
lakatoshv <some.pochta1@gmail.com>
2018-08-20 23:50:22 +0300

    Додана сторінка Контакти

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,7 @@ Route::get('posts/{id}', "PostsController@show");
 Route::get('/about', function () {
     return view('about');
 });
+Route::get('/contact', function () {
+    return view('contacts');
+});
 
