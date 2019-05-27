e0c5092d493e1ba1affd900521b89ddca74ed8aa
Віталій Лакатош <some.pochta1@gmail.com>
2019-05-27 15:39:50 +0300

    Search post in myPosts

--- resources/views/layouts/headerNavigation.blade.php
+++ resources/views/layouts/headerNavigation.blade.php
@@ -19,6 +19,11 @@
           <li class="nav-item"><a href="{{ route('login') }}" class="nav-link"><i class="fa fa-sign-in"></i> Ввійти</a></li>
           <li class="nav-item"><a href="{{ route('register') }}" class="nav-link"><i class="fa fa-user"></i> Зареєструватись</a></li>
         @else
+          <li class="nav-item">
+            <a href="{{url('myPosts')}}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+              Мої пости
+            </a>
+          </li>
           <li class="nav-item dropdow">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
               {{ Auth::user()->name }} <span class="caret"></span>
