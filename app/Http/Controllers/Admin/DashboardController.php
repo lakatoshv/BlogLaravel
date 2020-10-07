d24477c2060f938628f65d00a969b0518cac5770
Віталій Лакатош <some.pochta1@gmail.com>
2020-10-07 18:26:18 +0300

    Code refactoring in DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 
+/**
+ * Dashboard controller.
+ * 
+ * Display default admin pages.
+ */
 class DashboardController extends Controller
 {
-    public function dashboard(){
+    /**
+     * Show the admin dashboard.
+     *
+     * @return View
+     */
+    public function dashboard(): View {
     	return view("admin.dashboard");
     }
 }
