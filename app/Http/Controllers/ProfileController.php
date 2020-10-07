a4a327ff164b6a084bdfaa610e47252f6ce2b74e
Віталій Лакатош <some.pochta1@gmail.com>
2020-10-07 18:24:23 +0300

    Code refactoring in ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,9 +8,18 @@ use App\Models\Profile;
 use App\Models\Posts;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Profile controller.
+ * 
+ * Contains CRUD operations to work with Profile table.
+ */
 class ProfileController extends Controller
 {
-    
+    /**
+     * Display current user profile page.
+     *
+     * @return View
+     */
     public function index(){
         $profile = Profile::where('user_id', Auth::user()->id)->first();
         $posts = Posts::where('author', Auth::user()->id)->get();
@@ -18,9 +27,11 @@ class ProfileController extends Controller
         foreach ($posts as $post) {
             $post->author_id = $post->author;
             $author = DB::table('users')->where('id',$post->author) -> first();
-            if($author)
+            if($author) {
                 $post->author = $author->name;
+            }
         }
+
         return view('profile.index', compact('profile'), compact('posts'));
     }
 }
