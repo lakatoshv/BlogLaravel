bee91d8d80449ba0a5eb5865431b66197a437695
Віталій Лакатош <some.pochta1@gmail.com>
2020-12-23 16:47:52 +0200

    Fix update migration, delete columns on down

--- database/migrations/2020_11_12_203727_update_posts_table.php
+++ database/migrations/2020_11_12_203727_update_posts_table.php
@@ -36,6 +36,15 @@ class UpdatePostsTable extends Migration
      */
     public function down()
     {
-        
+        Schema::table("posts", function(Blueprint $table){
+            $table->dropForeign('user_id');
+            $table->dropForeign('category_id');
+
+            $table->dropColumn("category_id");
+            $table->dropColumn("user_id");
+            $table->dropColumn("excerpt");
+            $table->dropColumn("is_published");
+            $table->dropColumn("published_at");
+        });
     }
 }
